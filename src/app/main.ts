8b4d147bb48d18a098c5a7a0d2c214df21121e44
Pramod Nair <pramod.nair@open.edu.au>
2016-06-24 16:18:29 +1000

    set up base

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,24 +1,5 @@
-
-import { provide} from "@angular/core";
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ROUTER_PROVIDERS, ROUTER_PRIMARY_COMPONENT } from "@angular/router-deprecated";
-
-import {HTTP_PROVIDERS} from "@angular/http";
-import {APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from "@angular/common";
-
-/*
- *SERVICES
- */
-import {SPOTIFY_PROVIDERS} from "./services/spotify.service";
 
 import {AppComponent} from "./app.component";
 
-
-bootstrap(AppComponent, [
-  ROUTER_PROVIDERS,
-  HTTP_PROVIDERS,
-  SPOTIFY_PROVIDERS,
-  provide(ROUTER_PRIMARY_COMPONENT, {useValue: AppComponent}),
-  provide(APP_BASE_HREF, {useValue:'/'}),
-  provide(LocationStrategy, {useClass: HashLocationStrategy})
-]).catch((err: any)=> console.error(err));
+bootstrap(AppComponent).catch((err: any)=> console.error(err));
